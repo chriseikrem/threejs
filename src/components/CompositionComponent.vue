29f326b205913ddfbdd30b6966479f8133c3b4d2
Chris thomas Eikrem <christhomaseikrem@Chriss-MacBook-Pro.local>
2020-10-27 23:28:33 +0100

    Added possibility to drag elements

--- src/components/CompositionComponent.vue
+++ src/components/CompositionComponent.vue
@@ -21,6 +21,7 @@ import {
 import { Todo, Meta } from './models';
 import * as Three from 'three';
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import { DragControls } from 'three/examples/jsm/controls/DragControls';
 
 export default defineComponent({
   name: 'CompositionComponent',
@@ -30,7 +31,10 @@ export default defineComponent({
     const camera = ref();
     const scene = ref();
     const renderer = ref();
+    const dragControls = ref();
     const mesh = ref();
+    const meshesToRaycast = ref(Array<Three.Mesh>());
+    const selectedMesh = ref(Three.Mesh);
 
     const raycaster = ref(new Three.Raycaster());
     const mouse = ref(new Three.Vector2());
@@ -72,13 +76,14 @@ export default defineComponent({
         heightSegments
       );
       const material = new Three.MeshPhongMaterial({
-        color: 0xe6e6e6,
+        color: 0xf2f2f2,
         flatShading: false
       });
       const cylinder = new Three.Mesh(geometry, material);
 
       cylinder.position.set(posX, posY, posZ);
       cylinder.name = 'cylinder';
+      meshesToRaycast.value.push(cylinder);
 
       scene.value.add(cylinder);
     };
@@ -91,6 +96,8 @@ export default defineComponent({
         1000
       );
       camera.value.position.z = 5;
+      camera.value.position.y = 6;
+      camera.value.rotation.x = -45;
 
       scene.value = new Three.Scene();
 
@@ -124,54 +131,79 @@ export default defineComponent({
 
       renderer.value.setClearColor(0x45b6fe, 0.2);
 
-      const light = new Three.AmbientLight(0x404040);
+      /* const light = new Three.AmbientLight(0x404040);
+      scene.value.add(light); */
+
+      var light = new Three.PointLight(0xfff9db, 1, 400);
+      light.position.set(0, 75, 25);
       scene.value.add(light);
 
+      dragControls.value = new DragControls(
+        meshesToRaycast.value,
+        camera.value,
+        renderer.value.domElement
+      );
       document.body.prepend(renderer.value.domElement);
     };
 
     const animate = () => {
       requestAnimationFrame(animate);
 
-      raycaster.value.setFromCamera(mouse.value, camera.value);
-
-      // calculate objects intersecting the picking ray
-      var intersects = raycaster.value.intersectObjects(scene.value.children);
-
-      for (const intersect of intersects) {
-        // intersect.object.material.color.set(0xff0000)
-        // console.log(typeof intersect.object)
-        // intersect.object.rotation.x -= 0.02;
-        // intersect.object.rotation.y += 0.02;
-      }
       controls.value.update();
+
       renderer.value.render(scene.value, camera.value);
     };
 
     const onMouseMove = event => {
       event.preventDefault();
 
-      console.log(mouse.value.x);
-
-      var intersects = raycaster.value.intersectObjects(scene.value.children);
-      console.log('setup -> intersects', intersects);
-
       mouse.value.x = (event.clientX / window.innerWidth) * 2 - 1;
       mouse.value.y = -(event.clientY / window.innerHeight) * 2 + 1;
 
-      for (const intersect of intersects) {
+      raycaster.value.setFromCamera(mouse.value, camera.value);
+      const intersects = raycaster.value.intersectObjects(
+        meshesToRaycast.value
+      );
+      console.log('setup -> intersects', intersects);
+
+      if (intersects[0]) {
+        intersects[0].object.material.color.set(0xe0e0e0);
+      }
+
+      // selectedMesh.value = intersects[0].object;
+
+      /* for (const intersect of intersects) {
         //  console.log(intersect.object);
         //  intersect.object.material.color.set( 0xff0000 );
         if (intersect.object.name === 'cylinder') {
           intersect.object.material.color.set(0xf0000);
         }
-      }
+      }*/
     };
 
     init();
     animate();
 
     window.addEventListener('click', onMouseMove, false);
+
+    dragControls.value.addEventListener('dragstart', event => {
+      event.object.material.color.set(0xaaaaaa);
+      event.object.position.y = 0;
+    });
+    dragControls.value.addEventListener('drag', event => {
+      event.object.position.y = 0;
+    });
+    dragControls.value.addEventListener('dragend', event => {
+      event.object.material.color.set(0xf2f2f2);
+      controls.value.enabled = true;
+    });
+    dragControls.value.addEventListener('hoveron', event => {
+      event.object.material.color.set(0xf2f2f2);
+      controls.value.enabled = false;
+    });
+    dragControls.value.addEventListener('hoveroff', event => {
+      controls.value.enabled = true;
+    });
 
     return {};
   }
